db286dfb9d47b6cbf5b667bbbc586610afbc5c7d
Adrian <adrian.stypinski@gmail.com>
2021-03-31 12:52:50 +0200

    Project initialization

---
+++ src/main/java/pl/kzcwat/localincidentserver/LocalIncidentServerApplication.java
@@ -0,0 +1,13 @@
+package pl.kzcwat.localincidentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalIncidentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalIncidentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kzcwat/localincidentserver/LocalIncidentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kzcwat.localincidentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalIncidentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
